e91d224d78ccab107533123403b11c28c434ac0b
Yury Gargay <yury.gargay@gmail.com>
2015-07-22 19:35:25 +0600

    Add metricman

--- config/config.exs
+++ config/config.exs
@@ -6,3 +6,8 @@ use Mix.Config
 config :unimux,
   routes: [{"APIPrefix", 'http://127.0.0.1:8080'}],
   listen: 'zmq-tcp://127.0.0.1:20000'
+
+metricman_config = "deps/metricman/config/config.exs"
+if File.exists? metricman_config do
+  import_config "../" <> metricman_config
+end
